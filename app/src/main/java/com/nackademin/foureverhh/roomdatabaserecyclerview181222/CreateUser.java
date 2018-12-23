f76f829297052866e26a557c8e8c52e94c03129b
foureverhh <foureverhh@gmail.com>
2018-12-23 20:37:52 +0100

    Added all UI elements.

---
+++ app/src/main/java/com/nackademin/foureverhh/roomdatabaserecyclerview181222/CreateUser.java
@@ -0,0 +1,37 @@
+package com.nackademin.foureverhh.roomdatabaserecyclerview181222;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateUser extends AppCompatActivity {
+    private static final String TAG = "CreateUser";
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    Button button;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+        email = findViewById(R.id.email);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 //Save to the database
+                 Log.d(TAG,"OnClick:first name: "+firstName.getText().toString());
+            }
+        });
+        
+    }
+
+}
